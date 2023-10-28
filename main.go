f4380f2b2cc19af87c7fa3fb73a15e5ebae89290
sashagitar <reper.rre@gmail.com>
2023-10-28 21:46:25 +0300

    обновления для сдачи

--- main.go
+++ main.go
@@ -10,10 +10,10 @@ const api_tg_my = "5701189684:AAFFPRctBBKqjl-yRzo7sIy-hsg8cGApz_4"
 const api_tg = "5710143526:AAH6SoHEK_eFQJm_CNCHciYSaMgJl0sa4MQ"
 
 func main() {
-	fmt.Printf("%s\n", api_tg_my)
+	fmt.Printf("%s\n", api_tg)
 
 	// Создание бота
-	bot, err := tgbot.Create(api_tg_my)
+	bot, err := tgbot.Create(api_tg)
 	if err != nil {
 		panic(err)
 	}
